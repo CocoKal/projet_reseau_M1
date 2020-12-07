5be12f184f03f1fba115046f3dd434e42621575f
coren <corentin.camus@live.com>
2020-12-07 20:36:17 +0100

    Premier jet Partie 2

---
+++ VMs/partage/iftun.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include "iftun.h"
+
+
+
+int tun_alloc(char *dev) {
+  struct ifreq ifr;
+  int fd, err;
+
+  if( (fd = open("/dev/net/tun", O_RDWR)) < 0 ) {
+    perror("alloc tun");
+    exit(1);
+  }
+
+  memset(&ifr, 0, sizeof(ifr));
+
+  /* Flags: IFF_TUN   - TUN device (no Ethernet headers)
+   *        IFF_TAP   - TAP device
+   *
+   *        IFF_NO_PI - Do not provide packet information
+   */
+  ifr.ifr_flags = IFF_TUN;
+  if( *dev )
+    strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+
+  if( (err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0 ) {
+    close(fd);
+    return err;
+  }
+  strcpy(dev, ifr.ifr_name);
+  return fd;
+}
+
+void write_in_fd(int src, int dst) {
+  char buffer[1500];
+  int nread;
+  while(1) {
+
+    /* Note that "buffer" should be at least the MTU size of the interface, eg 1500   bytes */
+    nread = read(src,buffer,sizeof(buffer));
+
+    if(nread < 0) {
+      perror("Reading from interface");
+      break;
+    }
+
+    write(dst, buffer, nread);
+  }
+  close(src);
+  close(dst);
+
+}
